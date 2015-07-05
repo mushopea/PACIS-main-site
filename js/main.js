784a565a74aff7e91e310273a5db88ddc00de1e5
mushopea <mushopea@hotmail.com>
2015-07-05 21:14:20 +0800

    dr tan code

--- js/main.js
+++ js/main.js
@@ -1,25 +1,56 @@
 function viewMobileWebsite() {
-  $.cookie("view", 1);
-  window.location = "http://pacis2015.org/mobile/index.html";
+	$.cookie('layoutMode', 1, { expires: 1, path: '/' });
+    window.location.href = "http://pacis2015.comp.nus.edu.sg/mobile/index.html";
 }
 
 $(document).ready(function() {
-  if (screen.width < 600) {
-    $(".mobile-site-link").show();
-  } else {
-    // hiding mobile link
-    $(".mobile-site-link").hide();
-  }
-
-  console.log(screen.width );
-  if (screen.width < 600) {
-    if ($.cookie('view') == undefined) {
-      $.cookie("view", 1)
+    if ((/iPhone|iPod|iPad|Android|BlackBerry/).test(navigator.userAgent)) {
+        $(".mobile-site-link").show();
+    } else {
+        $(".mobile-site-link").hide();
     }
 
-    // if cookie is 1, direct to mobile website
-    if ($.cookie('view') == 1) {
-     window.location = "http://pacis2015.org/mobile/index.html";
-   }
-  }
+    console.log(screen.width);
+    if ((/iPhone|iPod|iPad|Android|BlackBerry/).test(navigator.userAgent)) {
+        if ($.cookie('layoutMode') == undefined) {
+			$.cookie('layoutMode', 1, { expires: 1, path: '/' });
+            console.log($.cookie('layoutMode') + " is initialized cookie");
+        }
+
+        console.log($.cookie('layoutMode') + " is the current cookie. Detected mobile in desktop.");
+
+        // if cookie is 1, direct to mobile website
+		var val = $.cookie('layoutMode');
+        if ($.cookie('layoutMode') == 1) {
+            console.log($.cookie('layoutMode') + " is the current cookie, redirecting to mobile. Val is " + val);
+            window.location.href = "http://pacis2015.comp.nus.edu.sg/mobile/index.html";
+        }
+    }
 });
+
+
+/*
+function viewMobileWebsite() {
+    window.location.href = "http://pacis2015.comp.nus.edu.sg/mobile/index.html";
+}
+
+$(document).ready(function() {
+	var url = window.location.href;
+
+	if(url == "http://pacis2015.comp.nus.edu.sg/" || url == "http://pacis2015.comp.nus.edu.sg/index.html")
+	{
+		var isMobileDevice = (/iPhone|iPod|iPad|Android|BlackBerry/).test(navigator.userAgent);
+		
+		if (isMobileDevice) {
+			$(".mobile-site-link").show();
+		} else {
+			// hiding mobile link
+			$(".mobile-site-link").hide();
+		}
+	
+		if (isMobileDevice) {
+		   window.location.href = "http://pacis2015.comp.nus.edu.sg/mobile/index.html";
+		}
+	}
+});
+*/
--- mobile/js/application.js
+++ mobile/js/application.js
@@ -2,140 +2,141 @@
  jQuery Mobile Boilerplate
  application.js
  */
-
 // MOBILE REDIRECTION CODE
 function viewFullWebsite() {
-  var now = new Date();
-  var expiresDate = new Date();
-  expiresDate.setDate(now.getDate() + 1);
-  $.cookie("view", 2, {
-    expires: expiresDate
-  });
-  if ($.cookie('view') == 2) {
-	  console.log('clicking on mobile website:');
-    window.location = "http://pacis2015.org/index.html";
-  }
+
+   $.cookie('layoutMode', 2, { expires: 1, path: '/' });
+    if ($.cookie('layoutMode') == 2) {
+        console.log('clicking on mobile website to go to main website');
+        console.log($.cookie('layoutMode') + " is the current cookie");
+        window.location = "http://pacis2015.comp.nus.edu.sg/index.html";
+    }
+
+	
+	window.location.href = "http://pacis2015.comp.nus.edu.sg/index.html?viewFullWebsite";
 }
 
 // Handler for navigating to the next page
 function navnext(next) {
-  $(":mobile-pagecontainer").pagecontainer("change", "#" + next, {
-    transition: "slide"
-  });
+    $(":mobile-pagecontainer").pagecontainer("change", "#" + next, {
+        transition: "slide"
+    });
 }
 
 // Handler for navigating to the previous page
 function navprev(prev) {
-  $(":mobile-pagecontainer").pagecontainer("change", "#" + prev, {
-    transition: "slide",
-    reverse: true
-  });
+    $(":mobile-pagecontainer").pagecontainer("change", "#" + prev, {
+        transition: "slide",
+        reverse: true
+    });
 }
 
 
 $(document).ready(function() {
-	if ($.cookie('view') == 2) {
-	  console.log("Redirecting to main website");
-	  window.location = "http://pacis2015.org/index.html";
-	}
+    console.log($.cookie('layoutMode') + " is the current cookie. In mobile site now.");
+
+    /*if ($.cookie('layoutMode') == 2) {
+      console.log("Redirecting to main website");
+      window.location = "http://pacis2015.org/index.html";
+    }*/
 
 
-  $(".panzoom").panzoom();
+    $(".panzoom").panzoom();
 
-  $(".goto").click(function() {
-    $("body").attr("go-to", $(this).attr("href"));
-  });
+    $(".goto").click(function() {
+        $("body").attr("go-to", $(this).attr("href"));
+    });
 
-  $(".go-to").on('click', function(e) {
-    e.preventDefault();
-    $(this).removeClass('ui-btn-active');
-    var goTo = $("body").attr("go-to") || "#programs";
-    window.location.href = window.location.pathname + goTo;
-  });
+    $(".go-to").on('click', function(e) {
+        e.preventDefault();
+        $(this).removeClass('ui-btn-active');
+        var goTo = $("body").attr("go-to") || "#programs";
+        window.location.href = window.location.pathname + goTo;
+    });
 
-  // Close the menu when click outisde
-  $(document).click(function(event) {
-    var div = $('.nav > ul');
-    if (!$(event.target).closest('.nav').length) {
-      if ($(div).is(":visible")) {
-        $(div).hide()
-      }
-    }
-  });
+    // Close the menu when click outisde
+    $(document).click(function(event) {
+        var div = $('.nav > ul');
+        if (!$(event.target).closest('.nav').length) {
+            if ($(div).is(":visible")) {
+                $(div).hide()
+            }
+        }
+    });
 
-  // toggle the menu with the button
-  $(".nav").on('click', function() {
-    var div = $('.nav > ul');
-    div.fadeToggle();
-  });
+    // toggle the menu with the button
+    $(".nav").on('click', function() {
+        var div = $('.nav > ul');
+        div.fadeToggle();
+    });
 
 
 });
 
 
 $(document).on("pagecreate", function() {
-  // animate accordion
-  $(".accordion-section .ui-collapsible-heading-toggle").on("click", function(e) {
-    var current = $(this).closest(".ui-collapsible");
-    if (current.hasClass("ui-collapsible-collapsed")) {
-      //collapse all others and then expand this one (disable collapse temporarily)
-      console.log("expanding and animating accordion");
-      //$(".ui-collapsible").not(".ui-collapsible-collapsed").find(".ui-collapsible-heading-toggle").click();
-      $(".ui-collapsible-content", current).slideDown(300);
-    } else {
-      $(".ui-collapsible-content", current).slideUp(300);
-    }
-  });
+    // animate accordion
+    $(".accordion-section .ui-collapsible-heading-toggle").on("click", function(e) {
+        var current = $(this).closest(".ui-collapsible");
+        if (current.hasClass("ui-collapsible-collapsed")) {
+            //collapse all others and then expand this one (disable collapse temporarily)
+            console.log("expanding and animating accordion");
+            //$(".ui-collapsible").not(".ui-collapsible-collapsed").find(".ui-collapsible-heading-toggle").click();
+            $(".ui-collapsible-content", current).slideDown(300);
+        } else {
+            $(".ui-collapsible-content", current).slideUp(300);
+        }
+    });
 
-  $(document).on("collapsibleexpand", "[data-role=collapsible]", function() {
-    var position = $(this).offset().top;
-    console.log("Accordion scoll to " + position);
-    $.mobile.silentScroll(position);
-  });
+    $(document).on("collapsibleexpand", "[data-role=collapsible]", function() {
+        var position = $(this).offset().top;
+        console.log("Accordion scoll to " + position);
+        $.mobile.silentScroll(position);
+    });
 
 });
 
 // Pagecreate will fire for each of the pages in this demo
 // but we only need to bind once so we use "one()"
 $(document).one("pagecreate", "body", function() {
-  // Navigate to the next page on swipeleft
-  $(document).on("swipeleft", ".ui-page", function(event) {
-    var next = $(this).jqmData("next");
-    navnext(next);
-  });
+    // Navigate to the next page on swipeleft
+    $(document).on("swipeleft", ".ui-page", function(event) {
+        var next = $(this).jqmData("next");
+        navnext(next);
+    });
 
-  // Navigate to the next page on swiperight
-  $(document).on("swiperight", ".ui-page", function(event) {
-    var prev = $(this).jqmData("prev");
-    navprev(prev);
-  });
+    // Navigate to the next page on swiperight
+    $(document).on("swiperight", ".ui-page", function(event) {
+        var prev = $(this).jqmData("prev");
+        navprev(prev);
+    });
 });
 
 $(document).on("pageshow", ".demo-page", function() {
-  var thePage = $(this),
-    title = thePage.jqmData("title"),
-    next = thePage.jqmData("next"),
-    prev = thePage.jqmData("prev");
-  // Point the "Trivia" button to the popup for the current page.
-  $("#trivia-button").attr("href", "#" + thePage.find(".trivia").attr("id"));
-  // We use the same header on each page
-  // so we have to update the title
-  $("#header h1").text(title);
-  // Prefetch the next page
-  // We added data-dom-cache="true" to the page so it won't be deleted
-  // so there is no need to prefetch it
-  if (next) {
-    $(":mobile-pagecontainer").pagecontainer("load", next + ".html");
-  }
-  // We disable the next or previous buttons in the footer
-  // if there is no next or previous page
-  // We use the same footer on each page
-  // so first we remove the disabled class if it is there
-  $(".next.ui-state-disabled, .prev.ui-state-disabled").removeClass("ui-state-disabled");
-  if (!next) {
-    $(".next").addClass("ui-state-disabled");
-  }
-  if (!prev) {
-    $(".prev").addClass("ui-state-disabled");
-  }
+    var thePage = $(this),
+        title = thePage.jqmData("title"),
+        next = thePage.jqmData("next"),
+        prev = thePage.jqmData("prev");
+    // Point the "Trivia" button to the popup for the current page.
+    $("#trivia-button").attr("href", "#" + thePage.find(".trivia").attr("id"));
+    // We use the same header on each page
+    // so we have to update the title
+    $("#header h1").text(title);
+    // Prefetch the next page
+    // We added data-dom-cache="true" to the page so it won't be deleted
+    // so there is no need to prefetch it
+    if (next) {
+        $(":mobile-pagecontainer").pagecontainer("load", next + ".html");
+    }
+    // We disable the next or previous buttons in the footer
+    // if there is no next or previous page
+    // We use the same footer on each page
+    // so first we remove the disabled class if it is there
+    $(".next.ui-state-disabled, .prev.ui-state-disabled").removeClass("ui-state-disabled");
+    if (!next) {
+        $(".next").addClass("ui-state-disabled");
+    }
+    if (!prev) {
+        $(".prev").addClass("ui-state-disabled");
+    }
 });
